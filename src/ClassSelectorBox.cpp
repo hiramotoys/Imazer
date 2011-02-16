1aab19731d56126a3aa08038c1dea62d0ea0e79a
hiramotoys <hiramoto.ys@gmail.com>
2011-02-16 20:09:24 +0900

    added ClassSelectorBox

---
+++ src/ClassSelectorBox.cpp
@@ -0,0 +1,44 @@
+#include "ClassSelectorBox.h"
+
+ClassSelectorBox::ClassSelectorBox(const QRect &rect, const QString &title, QWidget *iParent) : QGroupBox(title, iParent){
+    selectedObjID = -1;
+    setGeometry(rect);
+    layout = QHBoxLayout(this);
+    lines = new QRadioButton("Lines", layout);
+    connect(lines, SIGNAL(clicked()), this, SLOT(changeSelectedObjectIDToLines()));
+    green = new QRadioButton("Green", layout);
+    connect(green, SIGNAL(clicked()), this, SLOT(changeSelectedObjectIDToLines()));
+    ball = new QRadioButton("Ball", layout);
+    connect(ball, SIGNAL(clicked()), this, SLOT(changeSelectedObjectIDToLines()));
+    goalYellow = new QRadioButton("Goal Yellow", layout);
+    connect(goalYellow, SIGNAL(clicked()), this, SLOT(changeSelectedObjectIDToLines()));
+    goalBlue = new QRadioButton("Goal Blue", layout);
+    connect(goalBlue, SIGNAL(clicked()), this, SLOT(changeSelectedObjectIDToLines()));
+}
+
+ClassSelectorBox::~ClassSelectorBox(){
+}
+
+int ClassSelectorBox::getSelectedObjectID(){
+    return selectedObjID;
+}
+
+void ClassSelectorBox::changeSelectedObjectIDToLines(){
+    selectedObjID = NaoWorldObjectID::LINES;
+}
+
+void ClassSelectorBox::changeSelectedObjectIDToGreen(){
+    selectedObjID = NaoWorldObjectID::GREEN;
+}
+
+void ClassSelectorBox::changeSelectedObjectIDToBall(){
+    selectedObjID = NaoWorldObjectID::BALL;
+}
+
+void ClassSelectorBox::changeSelectedObjectIDToGoalYellow(){
+    selectedObjID = NaoWorldObjectID::GOAL_YELLOW;
+}
+
+void ClassSelectorBox::changeSelectedObjectIDToGoalBlue(){
+    selectedObjID = NaoWorldObjectID::GOAL_BLUE;
+}
---
+++ src/ClassSelectorBox.h
@@ -0,0 +1,34 @@
+#ifndef ClassSelectorBox_H
+#define ClassSelectorBox_H
+
+#include <QGroupBox>
+#include <QRect>
+#include <QString>
+#include <QWidget>
+#include <QRadioButton>
+#include <QHBoxLayout>
+#include "NaoWorldObjectID.h"
+
+class ClassSelectorBox : public QGroupBox{
+    Q_OBJECTS
+ private:
+    int selectedObjID;
+    QRadioButton *lines;
+    QRadioButton *green;
+    QRadioButton *ball;
+    QRadioButton *goalYellow;
+    QRadioButton *goalBlue;
+    QHBoxLayout *layout;
+ public:
+    ClassSelectorBox(const QRect &rect, const QString &title, QWidget *iParent=0);
+    ~ClassSelectorBox();
+    int getSelectedObjectID();
+ protected slots:
+    void changeSelectedObjectIDToLines();
+    void changeSelectedObjectIDToGreen();
+    void changeSelectedObjectIDToBall();
+    void changeSelectedObjectIDToGoalYellow();
+    void changeSelectedObjectIDToGoalBlue();
+};
+
+#endif // ClassSelectorBox
