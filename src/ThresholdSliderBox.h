fbe6709255ebb6972246ce62669ba15c603d366d
hiramotoys <hiramoto.ys@gmail.com>
2011-02-18 00:54:24 +0900

    added setter

--- src/ThresholdSliderBox.h
+++ src/ThresholdSliderBox.h
@@ -17,6 +17,9 @@ class ThresholdSliderBox : QGoupBox{
  public:
     ThresholdSliderBox(const int colorAttributeID, const QRect &rect, const QString &title, QWidget *iParent=0);
     ~ThresholdSliderBox();
+    void setFrValue(int fr);
+    void setToValue(int to);
+    void setValue(int fr, int to);
  protected slots:
     void changeThFr(int value);
     void changeThTo(int value);
