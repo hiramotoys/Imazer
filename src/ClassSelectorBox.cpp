4090c09839c96a19028de592b2a0d4f12e762dcd
hiramotoys <hiramoto.ys@gmail.com>
2011-02-22 15:58:20 +0900

    bug fix

--- src/ClassSelectorBox.cpp
+++ src/ClassSelectorBox.cpp
@@ -1,4 +1,5 @@
 #include "ClassSelectorBox.h"
+#include <iostream>
 
 ClassSelectorBox::ClassSelectorBox(const QRect &rect, const QString &title, QWidget *iParent) : QGroupBox(title, iParent){
     selectedObjID = -1;
@@ -8,16 +9,16 @@ ClassSelectorBox::ClassSelectorBox(const QRect &rect, const QString &title, QWid
     connect(lines, SIGNAL(clicked()), this, SLOT(changeSelectedObjectIDToLines()));
     layout->addWidget(lines);
     green = new QRadioButton("Green");
-    connect(green, SIGNAL(clicked()), this, SLOT(changeSelectedObjectIDToLines()));
+    connect(green, SIGNAL(clicked()), this, SLOT(changeSelectedObjectIDToGreen()));
     layout->addWidget(green);
     ball = new QRadioButton("Ball");
-    connect(ball, SIGNAL(clicked()), this, SLOT(changeSelectedObjectIDToLines()));
+    connect(ball, SIGNAL(clicked()), this, SLOT(changeSelectedObjectIDToBall()));
     layout->addWidget(ball);
-    goalYellow = new QRadioButton("Goal Yellow");
-    connect(goalYellow, SIGNAL(clicked()), this, SLOT(changeSelectedObjectIDToLines()));
+    goalYellow = new QRadioButton("GoalY");
+    connect(goalYellow, SIGNAL(clicked()), this, SLOT(changeSelectedObjectIDToGoalYellow()));
     layout->addWidget(goalYellow);
-    goalBlue = new QRadioButton("Goal Blue");
-    connect(goalBlue, SIGNAL(clicked()), this, SLOT(changeSelectedObjectIDToLines()));
+    goalBlue = new QRadioButton("GoalB");
+    connect(goalBlue, SIGNAL(clicked()), this, SLOT(changeSelectedObjectIDToGoalBlue()));
     layout->addWidget(goalBlue);
     setLayout(layout);
 }
@@ -31,20 +32,25 @@ int ClassSelectorBox::getSelectedObjectID(){
 
 void ClassSelectorBox::changeSelectedObjectIDToLines(){
     selectedObjID = NaoWorldObjectID::LINES;
+    std::cout << "lines " << selectedObjID << std::endl;
 }
 
 void ClassSelectorBox::changeSelectedObjectIDToGreen(){
     selectedObjID = NaoWorldObjectID::GREEN;
+    std::cout << "green " << selectedObjID << std::endl;
 }
 
 void ClassSelectorBox::changeSelectedObjectIDToBall(){
     selectedObjID = NaoWorldObjectID::BALL;
+    std::cout << "balls " << selectedObjID << std::endl;
 }
 
 void ClassSelectorBox::changeSelectedObjectIDToGoalYellow(){
     selectedObjID = NaoWorldObjectID::GOAL_YELLOW;
+    std::cout << "goaly " << selectedObjID << std::endl;
 }
 
 void ClassSelectorBox::changeSelectedObjectIDToGoalBlue(){
     selectedObjID = NaoWorldObjectID::GOAL_BLUE;
+    std::cout << "goalb " << selectedObjID << std::endl;
 }
