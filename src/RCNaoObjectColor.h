6a94435baca4aecb36c6186ea263d4c6c59b7058
hiramotoys <hiramoto.ys@gmail.com>
2011-03-01 13:31:28 +0900

    added RCNaoObjectColor

---
+++ src/RCNaoObjectColor.h
@@ -0,0 +1,13 @@
+#ifndef RCNaoObjectColor_H
+#define RCNaoObjectColor_H
+#include <QColor>
+
+namespace RCNaoObjectColor{
+    QColor LINES(255, 255, 255);
+    QColor GREEN(0, 255, 0);
+    QColor BALL(243, 152, 0);
+    QColor GOAL_YELLOW(255, 255, 0);
+    QColor GOAL_BLUE(0, 0, 255);
+}
+
+#endif // RCNaoObjectColor_H
